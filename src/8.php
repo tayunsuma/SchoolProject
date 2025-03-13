4b9d3689063ca2fe5ddfcc5dd0654f7664a997b1
tayunsuma <tayunsuma@gmail.com>
2025-03-13 10:42:10 +0000

    Add 8.php

---
+++ src/8.php
@@ -0,0 +1,12 @@
+<?php
+function getSchoolProjects() {
+    $projects = array(
+        "Math Project",
+        "Science Project",
+        "English Project",
+        "History Project",
+        "Technology Project"
+    );
+    return $projects;
+}
+?>
