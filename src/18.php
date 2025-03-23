908ca8e45f696ee8c7c01044e433921221ea3398
tayunsuma <tayunsuma@gmail.com>
2025-03-23 20:40:01 +0000

    Add 18.php

---
+++ src/18.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * This file is part of PHP School.
+ *
+ * @link     https://github.com/qqwen/php-school
+ * @copyright https://alibabacloud.com/aip/ Copyright (c) 2017 Alibaba Cloud
+ * @license  https://www.apache.org/licenses/LICENSE-2.0 Apache License Version 2.0
+ */
+
+/*
+ * Your PHP School project.
+ */
